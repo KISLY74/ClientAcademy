14e8108e7198833a1501b4cc27dfd8d1d0aa4fbf
KISLY74 <kisly.nikita@mail.ru>
2026-02-21 22:34:34 +0300

    add footer to others navs

--- src/pages/Lessons/Lessons.jsx
+++ src/pages/Lessons/Lessons.jsx
@@ -1,8 +1,11 @@
+import Footer from "../../components/Footer/Footer"
+
 export default function Lessons(){
 
     return <div className="lessons">
         <div className="lessons-container">
             
+             <Footer />
         </div>
     </div>
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,11 @@
+import Footer from "../../components/Footer/Footer"
+
 export default function Profile(){
 
     return <div className="profile">
         <div className="profile-container">
-            
+             
+             <Footer />
         </div>
     </div>
 }
